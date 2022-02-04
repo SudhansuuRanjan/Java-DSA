ff2f2ac9d73393949be964cd522d4a824f5d771b
SudhanshuuRanjan <sudhanshur.ug20.ee@nitp.ac.in>
2022-02-04 21:50:09 +0530

    learnt to create constructor

--- oops/src/com/sudhanshu/Main.java
+++ oops/src/com/sudhanshu/Main.java
@@ -3,6 +3,31 @@ package com.sudhanshu;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+       Student Student1 = new Student();
+
+        Student1.rno = 2002006;
+        Student1.name = "Sudhanshu Ranjan";
+        Student1.marks = (float) 8.56;
+
+        Student1.show();
+
     }
+
+    static class Student{
+        int rno ;
+        String name;
+        float marks;
+
+        Student(){
+
+        }
+
+        void show(){
+            System.out.println(rno);
+            System.out.println(name);
+            System.out.println(marks);
+        }
+    }
+
 }
